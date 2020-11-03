195dbf32b767504b1d261e32fd5bfdd655348732
ZackOrs <zachary_orsoli@hotmail.com>
2020-11-02 22:56:56 -0500

    Added a case for RemoveClient to the listener that works. We can follow this guideline for future buttons
    Note: right now the RemoveClient() needs main thread when doing stuff, so theres a bunch of methods but I'll clean that up

--- Form1.cs
+++ Form1.cs
@@ -19,6 +19,7 @@ namespace UdpCLientFinalForm
         IPEndPoint serverIpTest = null;
         CustomClient customClient = null;
         Thread SocketListenerThread = null;
+        String serverMessage = null;
 
         public Form1()
         {
@@ -69,14 +70,18 @@ namespace UdpCLientFinalForm
                 subjectGroupBox.Enabled = true;
                 clientOperationBox.Enabled = true;
 
+                bus = Encoding.ASCII.GetBytes("client " + customClient.ClientName + " : Checking Connection ...");
+                customClient.UdpClient.Send(bus, bus.Length);
+
+
                 //bus = Encoding.ASCII.GetBytes("client " + customClient.ClientName + " : Requesting connection ...");
-                //customClient.UdpClient.Send(bus, bus.Length, serverIpTest);
+                //customClient.UdpClient.Send(bus, bus.Length);
                 //bus = customClient.UdpClient.Receive(ref serverIpTest);
 
                 //bus = bus.Where(x => x != 0x00).ToArray(); // functions inspired from https://stackoverflow.com/questions/13318561/adding-new-line-of-data-to-textbox 
-                //string myString = Encoding.ASCII.GetString(bus).Trim();//see link on the aboce line
+                //string serverMessage = Encoding.ASCII.GetString(bus).Trim();//see link on the aboce line
 
-                //registeredUsersBox.Text += myString + Environment.NewLine;
+                //registeredUsersBox.Text += serverMessage + Environment.NewLine;
                 //customClient.UdpClient.Close();
                 Console.WriteLine("Exiting connection...");
 
@@ -111,38 +116,54 @@ namespace UdpCLientFinalForm
 
                         bus = customClient.UdpClient.Receive(ref serverIpTest);
                         bus = bus.Where(x => x != 0x00).ToArray(); // functions inspired from https://stackoverflow.com/questions/13318561/adding-new-line-of-data-to-textbox 
-                        string myString = Encoding.ASCII.GetString(bus).Trim();//see link on the aboce line
-                        UpdateRegisterUserTextBox("Connection good");
-                       
+                        serverMessage = Encoding.ASCII.GetString(bus).Trim();//see link on the aboce line
+                        UpdateRegisterUserTextBox(serverMessage);
+
                     }
                     catch (Exception ex)
                     {
-                        string myString = "Failure trying to receive message: " + ex;
+                        serverMessage = "Failure trying to receive message: " + ex;
                         Console.WriteLine("Failure trying to receive message: " + ex);
-                        UpdateRegisterUserTextBox("Connection failed");
-                        
+                        UpdateRegisterUserTextBox(serverMessage);
+
                     }
-                    Thread.Sleep(sleepVal);
+
+
+                    if (serverMessage.Equals("6"))
+                    {
+                        
+                        RemoveUser();
+                    }
+
+                    //reset the command
+                    serverMessage = null;
+                    //Thread.Sleep(sleepVal);
                     //customClient.Socket.Connect(serverIP);
+
+
+                }
+                else 
+                {
+                    Console.WriteLine("Null client");
                 }
             }
             
         }
 
         //Goes to main thread to update the textbox (otherwise crash)
-        delegate void SetTextCallback(string myString);
-        private void UpdateRegisterUserTextBox(string myString)
+        delegate void SetTextCallback(string serverMessage);
+        private void UpdateRegisterUserTextBox(string serverMessage)
         {
             
             if (registeredUsersBox.InvokeRequired)
             {
                 SetTextCallback d = new SetTextCallback(UpdateRegisterUserTextBox);
-                this.Invoke(d, new object[] { myString });
+                this.Invoke(d, new object[] { serverMessage });
             }
             else
             {
                 registeredUsersBox.Clear();
-                registeredUsersBox.Text += myString + Environment.NewLine;
+                registeredUsersBox.Text += serverMessage + Environment.NewLine;
 
             }
                 
@@ -211,9 +232,9 @@ namespace UdpCLientFinalForm
                     bus = clients.Last().UdpClient.Receive(ref serverIpTest);
 
                     bus = bus.Where(x => x != 0x00).ToArray(); // functions inspired from https://stackoverflow.com/questions/13318561/adding-new-line-of-data-to-textbox 
-                    string myString = Encoding.ASCII.GetString(bus).Trim();//see link on the aboce line
+                    serverMessage = Encoding.ASCII.GetString(bus).Trim();//see link on the aboce line
 
-                    if (Int64.Parse(myString) == 7)
+                    if (Int64.Parse(serverMessage) == 7)
                     {
                         richTextBox1.Text += String.Format("Update for {0} changed ip address to {1}:{2}", nameClientBox.Text, hostClientBox.Text, portClientBox.Text) + Environment.NewLine;
                         clients.Last().ChangeIP(hostClientBox.Text, Int32.Parse(portClientBox.Text));
@@ -244,34 +265,35 @@ namespace UdpCLientFinalForm
 
             try
             {
-                serverIpTest = new IPEndPoint(IPAddress.Parse("127.0.0.2"), 5080);
+                //serverIpTest = new IPEndPoint(IPAddress.Parse("127.0.0.2"), 5080);
                 string updateMsg = String.Format("DE-REGISTER,4,{0},{1},{2}", nameTextBox.Text, hostTextBox.Text, portTextBox.Text);
+                updateMsg = "abc";
                 bus = Encoding.ASCII.GetBytes(updateMsg);
-                clients.Last().RestartClient();
-                clients.Last().UdpClient.Send(bus, bus.Length, serverIpTest);
-                bus = clients.Last().UdpClient.Receive(ref serverIpTest);
+                //clients.Last().RestartClient();
+                clients.Last().UdpClient.Send(bus, bus.Length);
+                //bus = clients.Last().UdpClient.Receive(ref serverIpTest);
 
-                bus = bus.Where(x => x != 0x00).ToArray(); // functions inspired from https://stackoverflow.com/questions/13318561/adding-new-line-of-data-to-textbox 
-                string myString = Encoding.ASCII.GetString(bus).Trim();//see link on the aboce line
+                //bus = bus.Where(x => x != 0x00).ToArray(); // functions inspired from https://stackoverflow.com/questions/13318561/adding-new-line-of-data-to-textbox 
+                //string serverMessage = Encoding.ASCII.GetString(bus).Trim();//see link on the aboce line
 
-                if (Int64.Parse(myString) == 7)
-                {
-                    richTextBox1.Text += String.Format("Destroying for {0} changed ip address to {1}:{2}", nameTextBox.Text, hostTextBox.Text, portTextBox.Text) + Environment.NewLine;                   
-                    clients.Last().CloseConnection(serverIpTest);
-                    clients.Clear();
-                    hostTextBox.Enabled = true;
-                    nameTextBox.Enabled = true;
-                    portTextBox.Enabled = true;
+                //if (Int64.Parse(serverMessage) == 7)
+                //{
+                //    richTextBox1.Text += String.Format("Destroying for {0} changed ip address to {1}:{2}", nameTextBox.Text, hostTextBox.Text, portTextBox.Text) + Environment.NewLine;                   
+                //    clients.Last().CloseConnection(serverIpTest);
+                //    clients.Clear();
+                //hostTextBox.Enabled = true;
+                //nameTextBox.Enabled = true;
+                //portTextBox.Enabled = true;
 
-                    createButton.Enabled = true;
-                    removeButton.Enabled = false;
-                    subjectGroupBox.Enabled = false;
-                    clientOperationBox.Enabled = false;
-                }
-                else
-                {
-                    richTextBox1.Text += "Destruction rejected " + Environment.NewLine;
-                }
+                //createButton.Enabled = true;
+                //removeButton.Enabled = false;
+                //subjectGroupBox.Enabled = false;
+                //clientOperationBox.Enabled = false;
+                //}
+                //else
+                //{
+                //    richTextBox1.Text += "Destruction rejected " + Environment.NewLine;
+                //}
 
             }
             catch (Exception excep)
@@ -344,9 +366,9 @@ namespace UdpCLientFinalForm
                 bus = clients.Last().UdpClient.Receive(ref serverIpTest);
 
                 bus = bus.Where(x => x != 0x00).ToArray(); // functions inspired from https://stackoverflow.com/questions/13318561/adding-new-line-of-data-to-textbox 
-                string myString = Encoding.ASCII.GetString(bus).Trim();//see link on the aboce line
+                serverMessage = Encoding.ASCII.GetString(bus).Trim();//see link on the aboce line
 
-                if (Int64.Parse(myString) == 7)
+                if (Int64.Parse(serverMessage) == 7)
                 {
                     richTextBox1.Text += String.Format("subject list for {0} updated to {1}", subjectTextBox.Text, subjectList) + Environment.NewLine;
                 }
@@ -362,5 +384,112 @@ namespace UdpCLientFinalForm
             }
 
         }
+
+        private void RemoveUser()
+        {
+
+            UpdateRichTextBoxText(richTextBox1,
+                String.Format("Destroying for {0} changed ip address to {1}:{2}",
+                nameTextBox.Text, hostTextBox.Text, portTextBox.Text) + Environment.NewLine);
+
+            //richTextBox1.Text += String.Format("Destroying for {0} changed ip address to {1}:{2}", nameTextBox.Text, hostTextBox.Text, portTextBox.Text) + Environment.NewLine;
+            clients.Last().CloseConnection(serverIpTest);
+            clients.Clear();
+            UpdateTextBoxEnable(hostTextBox, true);
+            UpdateTextBoxEnable(nameTextBox, true);
+            UpdateTextBoxEnable(portTextBox, true);
+
+            UpdateButtonEnable(createButton, true);
+            UpdateButtonEnable(removeButton, false);
+
+            UpdateGroupBoxEnable(subjectGroupBox, false);
+            UpdateGroupBoxEnable(clientOperationBox, false);
+
+        }
+
+        //private void UpdateRichTextBox(string serverMessage)
+        //{
+
+        //    if (registeredUsersBox.InvokeRequired)
+        //    {
+        //        SetTextCallback d = new SetTextCallback(UpdateRichTextBox);
+        //        this.Invoke(d, new object[] { serverMessage });
+        //    }
+        //    else
+        //    {
+        //        richTextBox1.Clear();
+        //        richTextBox1.Text += serverMessage + Environment.NewLine;
+
+        //    }
+
+        //}
+
+
+
+
+        /// <summary>
+        /// Helper method to determin if invoke required, if so will rerun method on correct thread.
+        /// if not do nothing.
+        /// </summary>
+        /// <param name="c">Control that might require invoking</param>
+        /// <param name="a">action to preform on control thread if so.</param>
+        /// <returns>true if invoke required</returns>
+        public bool ControlInvokeRequired(Control c, Action a)
+        {
+            if (c.InvokeRequired) c.Invoke(new MethodInvoker(delegate { a(); }));
+            else return false;
+
+            return true;
+        }
+
+            // usage on textbox
+        public void UpdateRichTextBoxText(RichTextBox textbox, String text)
+        {
+            //Check if invoke requied if so return - as i will be recalled in correct thread
+            if (ControlInvokeRequired(textbox, () => UpdateRichTextBoxText(textbox, text))) return;
+                textbox.Text = text;
+        }
+        public void UpdateRichTextBoxEnable(RichTextBox textbox, bool value)
+        {
+            //Check if invoke requied if so return - as i will be recalled in correct thread
+            if (ControlInvokeRequired(textbox, () => UpdateRichTextBoxEnable(textbox, value))) return;
+            textbox.Enabled = value;
+        }
+
+        public void UpdateTextBoxText(TextBox textbox, String text)
+        {
+            //Check if invoke requied if so return - as i will be recalled in correct thread
+            if (ControlInvokeRequired(textbox, () => UpdateTextBoxText(textbox, text))) return;
+            textbox.Text = text;
+        }
+        public void UpdateTextBoxEnable(TextBox textbox, bool value)
+        {
+            //Check if invoke requied if so return - as i will be recalled in correct thread
+            if (ControlInvokeRequired(textbox, () => UpdateTextBoxEnable(textbox, value))) return;
+            textbox.Enabled = value;
+        }
+
+        //public void UpdateGroupBoxText(GroupBox groupBox, String text)
+        //{
+        //    //Check if invoke requied if so return - as i will be recalled in correct thread
+        //    if (ControlInvokeRequired(groupBox, () => UpdateGroupBoxText(groupBox, text))) return;
+        //    groupBox.Text = text;
+        //}
+        public void UpdateGroupBoxEnable(GroupBox groupBox, bool value)
+        {
+            //Check if invoke requied if so return - as i will be recalled in correct thread
+            if (ControlInvokeRequired(groupBox, () => UpdateGroupBoxEnable(groupBox, value))) return;
+            groupBox.Enabled = value;
+        }
+
+        //Or any control
+        public void UpdateButtonEnable(Button button,bool value)
+    {
+        //Check if invoke requied if so return - as i will be recalled in correct thread
+        if (ControlInvokeRequired(button, () => UpdateButtonEnable(button, value))) return;
+            button.Enabled = value;
+    }
+
+
     }
 }
