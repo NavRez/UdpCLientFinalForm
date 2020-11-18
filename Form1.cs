2fa962470e3b89c32b970fdcea9d64163240e004
ZackOrs <zachary_orsoli@hotmail.com>
2020-11-18 12:38:22 -0500

    whoops lmao

--- Form1.cs
+++ Form1.cs
@@ -396,7 +396,7 @@ namespace UdpCLientFinalForm
         private void ChangeServerReceived()
         {
             
-            if(servingServer == serverA)
+            if(servingServer.Equals(serverA))
             {
                 servingServer = serverB;
             }
